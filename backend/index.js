c137ca5cf32f5cb88c37ddc5b0b821ef29674e9c
MuddyEx <dmmahmudmuhd2468@gmail.com>
2025-04-07 17:32:14 +0100

    new update

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 // Middleware
 app.use(express.json({ limit: '50mb' }));
 app.use(cors({ 
-  origin: 'http://localhost:5173', 
+  origin: 'http://test.askdeepweb.com', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true 
 }));
